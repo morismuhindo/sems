14644a56f374339b48f3b40de4ca760c5325fb78
Muhindo Moris <161727603+morismuhindo@users.noreply.github.com>
2026-03-01 19:24:25 +0300

    Update Login.jsx

--- frontend/src/components/Pages/Auth/Login.jsx
+++ frontend/src/components/Pages/Auth/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch("/api/login", {
+      const res = await fetch("https://sems-backend-s2my.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ loginId, password })
