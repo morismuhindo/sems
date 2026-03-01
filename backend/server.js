5ecc406c0c2ae05d8d4b79fa1d1497120f32e8cb
Muhindo Moris <161727603+morismuhindo@users.noreply.github.com>
2026-03-01 13:33:41 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 // Express middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['https://sems-tu4u.onrender.com', 'http://localhost:3000'],
+  origin: ['https://sems-tu4u.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
